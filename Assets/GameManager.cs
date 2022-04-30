06bd46e19f4515aa9141a29bf34d9b0917a76b22
Fernando Garcia <fgarcg93@uoc.edu>
2022-04-30 10:23:26 +0200

    added scene and prefabs for game

---
+++ Assets/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public LevelBuilder m_LevelBuilder;
+    public GameObject m_NextButton;
+
+    private bool m_ReadyForInput;
+    public static bool ResetearScene = false;
+    private Player m_Player;
+
+    public GameObject Stage;
+
+
+    void Start() {
+
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("StageSelection"));
+
+        //  GameObject.Find("Canvas").GetComponent<SaveTest>().Stage.Stage = 0;
+        //   joystick = GameObject.Find("Fixed Joystick").transform ;
+
+        //  m_NextButton.SetActive(false);
+        ResetScene();
+      
+        //m_LevelBuilder.Build();
+        //m_Player = FindObjectOfType<Player>();
+       // //m_Player = GameObject.Find("PROTA(Clone)").GetComponent<Player>();
+    }
+
+    void Update() {
+
+        // IsLevelComplete(m_NextButton.SetActive(true));
+
+        if (ResetearScene)
+        {
+            ResetearScene = false;
+            ResetScene();
+        }
+
+       // Vector2 moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       // Vector2 moveInput = new Vector2(ProtaMove.MoveRight1() + ProtaMove.MoveLeft1(), ProtaMove.MoveUp1() + ProtaMove.MoveDown1());
+       // Vector2 moveInput = new Vector2(ProtaMove.MoveRight() + ProtaMove.MoveLeft(), ProtaMove.MoveUp() + ProtaMove.MoveDown());
+        moveInput.Normalize();
+
+        if (moveInput.sqrMagnitude > 0.5)
+
+        {
+
+            if (m_ReadyForInput)
+            {
+
+                m_ReadyForInput = false;
+                m_Player.Move(moveInput);
+              //  m_NextButton.SetActive(IsLevelComplete());
+
+            }
+            else
+            {
+
+                m_ReadyForInput = true;
+            }
+
+
+        }
+    
+    }
+
+
+    public void NextLevel() 
+    {
+      //  m_NextButton.SetActive(false);
+        m_LevelBuilder.NextLevel();
+       // LevelBuilder.m_CurrentLevel++;
+        // m_LevelBuilder.Build();
+        StartCoroutine(ResetSceneASync());
+    }
+    public void LevelX() 
+    {
+      //  m_NextButton.SetActive(false);
+        m_LevelBuilder.CargarStageX();
+       // LevelBuilder.m_CurrentLevel++;
+        // m_LevelBuilder.Build();
+        StartCoroutine(ResetSceneASync());
+    }
+
+    public void ResetScene()
+    {
+        StartCoroutine(ResetSceneASync());
+    }
+
+
+    bool IsLevelComplete() {
+
+        Box[] boxes = FindObjectsOfType<Box>();
+        foreach (var box in boxes)
+        {
+            if (!box.m_OnCross) return false;
+        }
+        
+        return true;
+
+    }
+
+    IEnumerator ResetSceneASync() {
+        if (SceneManager.sceneCount > 1)
+
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync("LevelScene");
+            while (!asyncUnload.isDone)
+            {
+                yield return null;
+                Debug.Log("Unlaoding...");
+            }
+            Debug.Log("Unload Done");
+            Resources.UnloadUnusedAssets();
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelScene", LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            Debug.Log("Laoding...");
+
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelScene"));
+        m_LevelBuilder.Build();
+        m_Player = FindObjectOfType<Player>();
+        Debug.Log("Level Loaded");
+    
+    }
+
+}
---
+++ Assets/LevelBuilder.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+[System.Serializable]
+public class LevelElement 
+{
+    public Text Level;
+    public string m_Character;
+    public GameObject m_Prefab;
+   
+
+}
+
+
+public class LevelBuilder : MonoBehaviour
+{
+    public static int m_CurrentLevel;
+    public Text Level;
+    public List<LevelElement> m_LevelElements;
+    private Level m_Level;
+    public static bool Construir = false;
+    public static bool Construir1 = false;
+    public static bool Construir2 = false;
+    public static int StageACargar;
+
+    GameObject GetPrefab(char c)
+    {
+        LevelElement levelElement = m_LevelElements.Find(le => le.m_Character == c.ToString());
+        if (levelElement != null)
+        {
+            return levelElement.m_Prefab;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void NextLevel()
+    {
+
+        m_CurrentLevel++;
+      //  SaveTest.ActualitzarStage();
+        if (m_CurrentLevel >= GetComponent<Levels>().m_Levels.Count)
+        {
+            m_CurrentLevel = 0;
+        }
+
+    }
+
+    public void Build()
+    {
+        //SceneManager.UnloadSceneAsync("StageSelection");
+
+        m_Level = GetComponent<Levels>().m_Levels[m_CurrentLevel];
+        int startx = -m_Level.Width / 2;
+        int x = startx;
+        int y = -m_Level.Height / 2;
+        foreach (var row in m_Level.m_Rows)
+        {
+            foreach (var ch in row)
+            {
+                Debug.Log(ch);
+                GameObject prefab = GetPrefab(ch);
+                if (prefab)
+                {
+                    Debug.Log(prefab.name);
+                    Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                x++;
+
+            }
+            y++;
+            x = startx;
+        }
+    }
+    public void Build1()
+    {
+
+        //m_CurrentLevel = 
+        m_Level = GetComponent<Levels>().m_Levels[m_CurrentLevel];
+        int startx = -m_Level.Width / 2;
+        int x = startx;
+        int y = -m_Level.Height / 2;
+        foreach (var row in m_Level.m_Rows)
+        {
+            foreach (var ch in row)
+            {
+                Debug.Log(ch);
+                GameObject prefab = GetPrefab(ch);
+                if (prefab)
+                {
+                    Debug.Log(prefab.name);
+                    Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                x++;
+
+            }
+            y++;
+            x = startx;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (PlayerData.Stage.Exists)
+        //{    CargarSavedGame();
+        //    m_CurrentLevel = PlayerData.Stage.Stage;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Construir)
+        {
+            Construir = false;
+            GameManager.ResetearScene = true;
+            Build1();
+
+
+        } 
+        if (Construir1)
+        {
+            Construir1 = false;
+            GameManager.ResetearScene = true;
+            Build1();
+
+
+        }
+        if (Construir2)
+        {
+            Construir2 = false;
+            CargarStageX();
+            //GameManager.ResetearScene = true;
+            //Build1();
+
+
+        }
+      //  PlayerData.Stage) = m_CurrentLevel;
+
+        //Level = m_CurrentLevel.ToString();
+        //GameObject.Find("NumStage").GetComponent<Text>().TextMesh = Level.ToString();
+    }
+    
+    public static void CargarSavedGame()
+    {
+        m_CurrentLevel = GameObject.Find("Canvas").GetComponent<SaveTest>().Stage.Stage;
+        Resources.UnloadUnusedAssets();
+        // m_CurrentLevel = int.Parse(SaveTest.Stage);
+        Construir = true;
+        // Resources.UnloadUnusedAssets();
+
+      //  StartCoroutine (GameManager.ResetSceneASync());
+        // GameManager.ResetScene();
+
+    }
+
+
+    public void CargarStageX()
+    {
+        m_CurrentLevel = StageACargar  ;
+      //  Construir1 = true;
+       // if (StageACargar == 1) { LevelBuilder.m_CurrentLevel = 1; LevelBuilder.CargarSavedGame(); }
+
+    }
+}
